be2e866e77c59217ef5e9fe045a0b3b27757952f
Jay <jkb5678@gmail.com>
2021-05-26 15:15:38 +0530

    Dummy change

--- treenotesapp/settings.py
+++ treenotesapp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ov($00^2r!el22=z=ee_+_kwe$fcich-ib$(j@#(v)%tus%eg3'
+SECRET_KEY = 'ov($00^2r!el22=z=ee_+_kwjksd83isdjh45e$fcich-ib$(j@#(v)%tus%eg3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
