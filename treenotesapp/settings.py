cb02248b9969e43756c8a65b587e351df04e1735
Jay <jkb5678@gmail.com>
2021-05-27 15:24:54 +0530

    final

--- treenotesapp/settings.py
+++ treenotesapp/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ov($00^2r!el22=z=ee_+_kwjksd83isdjh45e$fcich-ib$(j@#(v)%tus%eg3'
+with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
